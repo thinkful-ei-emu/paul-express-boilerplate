12e233ba50bef35f66bc0e88d411a24cad4da67f
Paul Han <kyutae.han93@gmail.com>
2019-07-18 11:00:28 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+const { PORT } = require('./config')
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT} running in ${process.env.NODE_ENV} mode...`);
+});
